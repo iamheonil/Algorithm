6db1ced150b6cfae47c3b2e3cc5fe1149c5bd5dd
KIM HEONIL <iamheonil@gmail.com>
2020-11-03 21:29:50 +0900

    1일 1알고리즘 문제풀기 (A / B 를 출력하시오)

---
+++ src/list/Baekjoon1008.java
@@ -0,0 +1,24 @@
+package list;
+
+import java.util.Scanner;
+
+public class Baekjoon1008 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("수를 입력하세요");
+        int a = sc.nextInt();
+
+        sc.nextLine();
+
+        System.out.print("다음 수를 입력하세요 \n");
+        int b = sc.nextInt();
+
+        System.out.print(a / (double) b);
+
+    }
+}
+
+// 때에 따라서 형변환을 해줘야할 때도 있다는 것을 인지해야한다.
+// 상황에 따라서 유연하게.
