7f02dc30b727579be983c40b94406f37260d5ff2
KIM HEONIL <iamheonil@gmail.com>
2020-11-22 12:44:55 +0900

    1일 1알고리즘 (자연수 N이 주어졌을 때, N부터 1까지 한 줄)

---
+++ src/list/Baekjoon2742.java
@@ -0,0 +1,25 @@
+package list;
+
+import java.util.Scanner;
+
+public class Baekjoon2742 {
+
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("N의 수를 입력해주세요 : ");
+        int n = sc.nextInt();
+
+        for (int i = n; i >= 1; i-- ) {
+            System.out.println(i);
+        }
+
+        // 기찍 N
+        // 자연수 N이 주어졌을 때, N부터 1까지 한 줄에 하나씩 출력하는 프로그램을 작성하시오.
+
+
+    }
+
+}
