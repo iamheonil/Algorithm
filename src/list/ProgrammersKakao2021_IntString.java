92dcb841b766f9016fb0f4bebd6d9cd3ecd28a95
KIM HEONIL <iamheonil@gmail.com>
2021-08-22 20:54:05 +0900

    프로그래머스 2021년도 카카오 채용연계형 인턴십 - 숫자 문자열과 영단어

---
+++ src/list/ProgrammersKakao2021_IntString.java
@@ -0,0 +1,23 @@
+package list;
+
+public class ProgrammersKakao2021_IntString {
+
+    public static int solution(String s) {
+
+        String[] numInt = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+        String[] numStr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+        for(int i = 0; i < 10; i++) {
+            s = s.replace(numStr[i], numInt[i]);
+        }
+        return Integer.parseInt(s);
+    }
+
+    public static void main(String[] args) {
+
+        int s = solution("one4seveneight");
+
+        System.out.print(s);
+        
+    }
+}
