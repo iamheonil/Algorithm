5f4e0e063d65cbb6738f7e5e82ef60c4fa6cf8de
KIM HEONIL <iamheonil@gmail.com>
2021-08-31 22:56:38 +0900

    프로그래머스 평균 구하기 (Double)

---
+++ src/list/Programmers_Average.java
@@ -0,0 +1,28 @@
+package list;
+
+public class Programmers_Average {
+
+    public static double solution(int[] s) {
+
+        // 결과를 저장할 double 변수
+        double answer = 0;
+
+        // 문자열로 넘어온 s를 함수를 사용하여 answer에 담아준다
+        for (int i = 0; i < s.length; i++) {
+            answer += s[i];
+        }
+
+        // answer에 담긴 값 리턴
+        return answer / s.length;
+
+    }
+
+    public static void main(String[] args) {
+
+        // 파라미터
+        double result = solution(new int[]{1, 2, 3, 4});
+
+        // 솔루션 값 출력
+        System.out.print(result);
+    }
+}
