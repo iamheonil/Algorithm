2b42f6ccb80cecda60da95d3f54932e1cea70f9e
KIM HEONIL <iamheonil@gmail.com>
2021-09-06 21:50:08 +0900

    프로그래머스 정수 내림차순 정렬(코드 주석 추가)

--- src/list/Programmers_AscNumber.java
+++ src/list/Programmers_AscNumber.java
@@ -6,14 +6,19 @@ import java.util.Collections;
 public class Programmers_AscNumber {
 
     public static long solution(long n) {
-
+        // 결과를 출력할 long 변수
         long answer = 0;
 
+        // 내림차순 작업을 할 String 배열
         String[] parArr = ("" + n).split("");
 
+        // 배열 정렬 (오름차순)
         Arrays.sort(parArr);
+
+        // 오름차순 정렬 완료 된 배열을 거꾸로
         Collections.reverse(Arrays.asList(parArr));
 
+        // long 형 변수에 String.join 함수를 사용하여 혹여나 들어가 있을지도 모르는 공백 및 ',' 제거
         answer = Long.parseLong(String.join("", parArr));
 
         // 결과 출력
