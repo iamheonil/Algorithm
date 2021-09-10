fd1e87604c55e189a0926a286d6e29beb82a72f5
KIM HEONIL <iamheonil@gmail.com>
2021-09-11 03:05:06 +0900

    프로그래머스 자연수 뒤집어 배열로 만들기 해답 - 주석추가

--- src/list/Programmers_NumberDescArray.java
+++ src/list/Programmers_NumberDescArray.java
@@ -7,15 +7,19 @@ public class Programmers_NumberDescArray {
 
     public static int[] solution(long n) {
 
+        // 파라미터로 넘어온 long형 변수 n을 String형으로 파싱한다.
         String parseLong = Long.toString(n);
 
+        // 파싱이 완료된 parseLong은 longArr이라는 String 배열에 넣어준다.
         String[] longArr = parseLong.split("");
 
+        // 작업이 끝난 longArr 함수는 Collections의 reverse 함수를 사용하여 뒤집는다.
         Collections.reverse(Arrays.asList(longArr));
 
-        // 결과를 출력할 int 배열
+        // 결과를 출력할 int 배열 생성
         int[] answer = new int[longArr.length];
 
+        // answer 배열에 longArr 배열의 값을 담아준다.
         for (int i = 0; i < answer.length; i++) {
             answer[i] = Integer.parseInt(longArr[i]);
         }
@@ -30,7 +34,7 @@ public class Programmers_NumberDescArray {
         // 파라미터
         int[] result = solution(12345);
 
-        // 솔루션 값 출력
+        // 씨스아웃 (ㅋㄷㅋㄷ)
         System.out.print(Arrays.toString(result));
     }
 }
