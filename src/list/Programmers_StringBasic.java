24fac824a23a7b32d32a1a43d07577e450be5218
KIM HEONIL <iamheonil@gmail.com>
2021-09-13 23:30:48 +0900

    문자열 다루기 기본 주석 추가

--- src/list/Programmers_StringBasic.java
+++ src/list/Programmers_StringBasic.java
@@ -4,13 +4,17 @@ public class Programmers_StringBasic {
 
     public static boolean solution(String s) {
 
-        // 문자열로 넘어온 s를 answer에 담아준다
+        // String s의 길이가 4 혹은 6인지 확인한다.
         if (s.length() == 4 || s.length() == 6) {
+
+            // 4 혹은 6이라면, 반복문을 돌려 넘어온 문자열에 숫자 0~9 이외의 값이 있다면
             for (int i = 0; i < s.length(); i++) {
                 char ch = s.charAt(i);
                 if (ch < '0' || ch > '9')
+                    // false 출력
                     return false;
             }
+            // 숫자만 들어가 있다면 true 출력
             return true;
         } else
             return false;
