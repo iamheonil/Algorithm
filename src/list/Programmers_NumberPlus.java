f123845422198eb032fbfeaa46d6d90ae31bf5ec
KIM HEONIL <iamheonil@gmail.com>
2021-09-14 21:41:52 +0900

    프로그래머스 두 정수 사이의 합 풀이 주석 추가

--- src/list/Programmers_NumberPlus.java
+++ src/list/Programmers_NumberPlus.java
@@ -9,17 +9,21 @@ public class Programmers_NumberPlus {
         // 결과를 출력할 long 변수
         long answer = 0;
 
+        // max값과 min값을 담아놓을 CompNum, lowNum 생성
         int compNum = 0;
         int lowNum = 0;
 
+        // 파라미터로 넘어온 a가 b보다 작다면?
         if(a < b) {
             compNum = b;
             lowNum = a;
         } else {
+            // a가 b보다 크다면?
             compNum = a;
             lowNum = b;
         }
 
+        // 위에서 정리된 compNum과 lowNum 체크해서 반복문 실행
         for (int i = compNum; lowNum <= compNum; i++) {
             answer += lowNum;
             lowNum++;
