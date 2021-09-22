5f0c4dc7c01a8e5e0c7589a5a15848edeaab4f9d
KIM HEONIL <iamheonil@gmail.com>
2021-09-22 23:03:15 +0900

    프로그래머스 하샤드 수 문제 풀이 중 (주석 추가)

--- src/list/Programmers_HarshadNumber.java
+++ src/list/Programmers_HarshadNumber.java
@@ -3,22 +3,30 @@ package list;
 public class Programmers_HarshadNumber {
 
     public static boolean solution(int n) {
+        // 결과를 반환할 answer 선언
         boolean answer = true;
 
+        // 하샤드 수인지 확인할 파라미터 변수
         int harshadNum = 0;
 
+        // 파라미터로 넘어온 n값을 배열로 생성해주기 위한 사전 작업(?)
         String param = Integer.toString(n);
 
+        // 파라미터로 넘어온 n값을 더해주기 위해 배열을 생성해준다
         String[] parseArr = param.split("");
 
+        // 반복문 실행
         for (int i = 0; i < parseArr.length; i++) {
+            // 하샤드수에 파라미터를 더해준다
             harshadNum += Integer.parseInt(parseArr[i]);
         }
 
+        // 파라미터(n)와 파라미터를 나눈수가 나누어 떨어진다면 하샤드 수
         if (n % harshadNum != 0) {
             answer = false;
         }
 
+        // 결과 리턴
         return answer;
     }
 
