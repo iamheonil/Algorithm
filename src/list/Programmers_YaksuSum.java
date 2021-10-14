60b85f2de848ec2d4e558e01f34f703c03d87ddd
KIM HEONIL <iamheonil@gmail.com>
2021-10-14 22:04:24 +0900

    프로그래머스 약수의 합 문제 풀이 (주석 추가)

--- src/list/Programmers_YaksuSum.java
+++ src/list/Programmers_YaksuSum.java
@@ -6,8 +6,11 @@ public class Programmers_YaksuSum {
 
         int answer = 0;
 
+        // 반복문 > 1부터 n까지
         for (int i = 1; i <= n; i++) {
+            // n과 i을 나눴을 때 결과가 0이라면?!
             if (n % i == 0) {
+                // 약수라고 판단, answer에 담아준다!
                 answer += i;
             }
         }
@@ -19,7 +22,7 @@ public class Programmers_YaksuSum {
     public static void main(String[] args) {
 
         // 파라미터
-        int s = solution(12);
+        int s = solution(5);
 
         // 리턴 값 씨스아웃
         System.out.print(s);
