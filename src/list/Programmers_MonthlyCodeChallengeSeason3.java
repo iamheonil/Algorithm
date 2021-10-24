9f7df70fe4f0489cf183384bdca5c87a69b601dd
KIM HEONIL <iamheonil@gmail.com>
2021-10-24 19:46:15 +0900

    프로그래머스 월간 코드 챌린지 시즌 3 나머지가 1이 되는 수 찾기

---
+++ src/list/Programmers_MonthlyCodeChallengeSeason3.java
@@ -0,0 +1,29 @@
+package list;
+
+public class Programmers_MonthlyCodeChallengeSeason3 {
+
+    public static int solution(int n) {
+
+        int i;
+
+        // 매개변수로 받은 n과 i(1씩 증가)를 나누기 위해 반복문을 작성
+        for (i = 1; i < n; i++) {
+
+            // n과 i를 나눴을 때, 나머지가 1이라면 i를 반환한다.
+            if (n % i == 1) {
+                return i;
+            }
+        }
+        
+        return i;
+    }
+
+    public static void main(String[] args) {
+
+        // 파라미터
+        int result = solution(12);
+
+        // 솔루션 값 출력
+        System.out.print(result);
+    }
+}
