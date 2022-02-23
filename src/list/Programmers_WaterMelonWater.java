c68be20e29f3589acd75bb81b31d11c8e871f737
KIM HEONIL <iamheonil@gmail.com>
2022-02-24 00:12:30 +0900

    프로그래머스 수박수박수박수박수박수.. 문제 Java 정답

--- src/list/Programmers_WaterMelonWater.java
+++ src/list/Programmers_WaterMelonWater.java
@@ -2,18 +2,16 @@ package list;
 
 public class Programmers_WaterMelonWater {
 
-    public static String solution(int n) {
+    public static String solution(int s) {
 
-        String answer = "";
+        String answer = "수";
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < s; i++) {
             // 문자열이 비어있거나, 끝자리가 박일경우 수를 넣어주고
             if (answer.isEmpty() || "박".equals(answer.substring(answer.length() - 1))) {
                 answer += "수";
-            } else {
-                // 아닐경우는 모두 박
-                answer += "박";
             }
+
         }
 
         // 결과 리턴
@@ -23,7 +21,7 @@ public class Programmers_WaterMelonWater {
     public static void main(String[] args) {
 
         // 파라미터
-        String s = solution(32);
+        String s = solution(4);
 
         // 결과 출력
         System.out.print(s);
