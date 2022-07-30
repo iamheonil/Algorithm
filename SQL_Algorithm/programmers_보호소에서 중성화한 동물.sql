25c7c75f8b79cff5192cff1d1e6cf4d7c44f33ed
KIM HEONIL <iamheonil@gmail.com>
2022-07-30 21:34:28 +0900

    프로그래머스 SQL 고득점 Kit_보호소에서 중성화한 동물

---
+++ SQL_Algorithm/programmers_보호소에서 중성화한 동물.sql
@@ -0,0 +1,8 @@
+-- 코드를 입력하세요
+SELECT A.ANIMAL_ID, A.ANIMAL_TYPE, A.NAME FROM ANIMAL_INS A, ANIMAL_OUTS B
+WHERE A.ANIMAL_ID = B.ANIMAL_ID
+AND A.SEX_UPON_INTAKE LIKE '%Intact%'
+AND B.SEX_UPON_OUTCOME NOT IN ('Intact Male', 'Intact Female');
+
+
+--AND B.SEX_UPON_OUTCOME IN ('Spayed Male', 'Spayed Female', 'Neutered Male', 'Neutered Female');
