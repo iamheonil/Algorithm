e7ad43002b96474913d23b10dc7628da6bf6b3e0
Kim heonil <iamheonil@gmail.com>
2023-10-06 11:22:02 +0900

    프로그래머스 코딩 기초 트레이닝 문제 풀이 시작

---
+++ src/list/Programmers_toLowerCase.java
@@ -0,0 +1,18 @@
+package list;
+
+public class Programmers_toLowerCase {
+
+    public static String solution(String myString) {
+        return myString.toLowerCase();
+    }
+
+    public static void main(String[] args) {
+
+        // 파라미터
+        String result = solution("aBcDeFg");
+
+        // 솔루션 값 출력
+        System.out.print(result);
+
+    }
+}
